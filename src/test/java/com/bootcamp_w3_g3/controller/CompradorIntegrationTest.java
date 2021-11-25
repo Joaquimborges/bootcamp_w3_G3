90696f136520b70d69987edf9195b4d1990d63ef
joaquimborges <joaquim.borges1993@gmail.com>
2021-11-25 20:51:51 -0300

    CompradorIntegrationTest: montado o teste dos endpoints da carteira, funcionando 100%

--- src/test/java/com/bootcamp_w3_g3/controller/CompradorIntegrationTest.java
+++ src/test/java/com/bootcamp_w3_g3/controller/CompradorIntegrationTest.java
@@ -63,7 +63,29 @@ public class CompradorIntegrationTest {
                 .build();
     }
 
-    private void persisteComprador(CompradorForm compradorForm){
+    private CompradorForm payloadComprador3(){
+        return CompradorForm.builder()
+                .codigo("C-3")
+                .nome("Bruno")
+                .sobrenome("Henrique")
+                .endereco("Rua da Libertadores 2021")
+                .cpf("12312325392")
+                .telefone("21 3425-7856")
+                .build();
+    }
+
+    private CompradorForm payloadComprador4(){
+         return CompradorForm.builder()
+                .codigo("C-4")
+                .nome("Bruno")
+                .sobrenome("Henrique")
+                .endereco("Rua da Libertadores 2021")
+                .cpf("19362312392")
+                .telefone("21 3425-7856")
+                .build();
+    }
+
+    private Comprador persisteComprador(CompradorForm compradorForm){
         Comprador comprador = Comprador.builder()
                 .codigo(compradorForm.getCodigo())
                 .nome(compradorForm.getNome())
@@ -73,7 +95,7 @@ public class CompradorIntegrationTest {
                 .endereco(compradorForm.getEndereco())
                 .build();
 
-        this.compradorService.salvar(comprador);
+        return compradorService.salvar(comprador);
     }
 
     /**
@@ -112,5 +134,41 @@ public class CompradorIntegrationTest {
                 .content(requestPayload))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.nome", is(compradorAlterado.getNome())));
+    }
+
+
+    /**
+     *Teste deve salvar um comprador, criar uma carteira
+     * no momento que o comprador for instanciado.
+     *
+     * Fazer a requisição no endpoint e realizar o depósito
+     * na carteira do comprador.
+     */
+    @Test
+    void deveDepositarValorNaCarteiraDoComprador() throws Exception {
+        double saldo = 1000.0;
+        CompradorForm compradorForm = payloadComprador3();
+        Comprador comprador = persisteComprador(compradorForm);
+
+        this.mockMvc.perform(get("http://localhost:8080/comprador/carteira/depositar/"
+                        + comprador.getCarteira().getNumero() + "/" + saldo))
+                .andExpect(status().isOk());
+    }
+
+    /**
+     *Teste deve salvar um comprador
+     * instanciar uma carteira.
+     *
+     * Fazer a requisição e consultar o saldo da carteira
+     */
+    @Test
+    void deveConsultarSaldoDaCarteira() throws Exception {
+        CompradorForm compradorForm = payloadComprador4();
+        Comprador comprador = this.persisteComprador(compradorForm);
+
+        this.mockMvc.perform(get("http://localhost:8080/comprador/carteira/saldo/"
+                        + comprador.getCarteira().getNumero()))
+                .andExpect(status().isOk());
+
     }
 }
